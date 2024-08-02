47ba44d15aea21d118cdc2ae8f907b7a9f60dc11
Adam <adam.mabrouki@webwag.com>
2024-08-02 11:31:09 +0200

    Initial Commit

---
+++ Honey-Pote/Honey_PoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Honey_PoteApp.swift
+//  Honey-Pote
+//
+//  Created by Adam Mabrouki on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Honey_PoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
