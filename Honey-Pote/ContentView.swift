2f3e4d115e8c4f53b296adff04ece61a32b66e67
Adam <adam.mabrouki@webwag.com>
2024-08-02 11:31:09 +0200

    Initial Commit

---
+++ Honey-Pote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Honey-Pote
+//
+//  Created by Adam Mabrouki on 02/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
